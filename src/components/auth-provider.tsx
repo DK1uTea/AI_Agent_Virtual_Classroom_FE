e97550330c38a0dd367c66eedf3bf720a77b29bd
Do Quang Tuan <quangtuan9102003@gmail.com>
2025-10-21 10:27:04 +0700

    feat: Implement access token auto-renewal
    - Install the dayjs library dependency.
    - Add dayjs dependency.
    - Set up an interval to automatically refresh the access token before it expires.
    - Add a utility function to decode JSON Web Tokens.

--- src/components/auth-provider.tsx
+++ src/components/auth-provider.tsx
@@ -4,6 +4,13 @@ import { authApis } from "@/apis/gateways/auth-apis";
 import { useAuthStore } from "@/stores/auth-store";
 import { useEffect } from "react";
 import { useShallow } from "zustand/shallow";
+import dayjs from "dayjs";
+import { decodeJwt, forcedSignOut } from "@/lib/utils";
+import { decode } from "punycode";
+import { access } from "fs";
+
+const REFRESH_INTERVAL_MINUTES = 5
+const REFRESH_THRESHOLD_MINUTES = 5
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const {
@@ -43,6 +50,43 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   useEffect(() => {
     fetchAuth();
   }, []);
+
+  useEffect(() => {
+
+    if (!accessToken) return;
+
+    const interval = setInterval(async () => {
+      const now = dayjs();
+
+      const decodedAccessToken = decodeJwt(accessToken);
+
+      if (decodedAccessToken && decodedAccessToken.exp) {
+        const expiresAt = dayjs.unix(decodedAccessToken.exp);
+        const minutesToExpire = expiresAt.diff(now, 'minute');
+        console.log(`Access token expires in ${minutesToExpire} minutes`);
+
+        if (minutesToExpire <= REFRESH_THRESHOLD_MINUTES) {
+          try {
+            const res = await authApis.refreshNextServer();
+            console.log("Token refreshed successfully in AuthProvider!");
+            setAuthState(true, user, res.accessToken, res.refreshToken);
+          } catch (error) {
+            console.error("Error refreshing token in AuthProvider: ", error);
+            forcedSignOut();
+            setAuthState(false, {
+              userId: null,
+              username: '',
+              email: ''
+            }, '', '');
+          }
+        }
+
+      }
+
+    }, 1000 * 60 * REFRESH_INTERVAL_MINUTES);
+
+    return () => clearInterval(interval);
+  }, [accessToken]);
 
   return <>
     {children}
