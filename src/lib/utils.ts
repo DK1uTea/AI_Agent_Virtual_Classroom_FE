0abd2bbe4e21e289704829b745777ea8c9910d04
Do Quang Tuan <quangtuan9102003@gmail.com>
2025-11-04 15:06:57 +0700

    feat: Implement lesson quiz functionality.
    - Implement the `BeforeStartQuiz` component to display quiz instructions and initiate the quiz session.
    - Implement the initial structure, state management, and basic components for a quiz page, including conditional rendering for the start screen, a timer display, progress bar, question card layout, and navigation buttons.
    - Implement the Alert component including title, description, and variant styles.
    - Add a utility function to format seconds into a standardized MM:SS or HH:MM:SS timer display string.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -48,3 +48,19 @@ export const decodeJwt = (token: string) => {
     return null
   }
 }
+
+export const formatTimer = (seconds: number): string => {
+  const hours = Math.floor(seconds / 3600);
+  const mins = Math.floor((seconds % 3600) / 60);
+  const secs = seconds % 60;
+
+  if (hours > 0) {
+    return `${hours.toString().padStart(2, '0')}:${mins
+      .toString()
+      .padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
+  } else {
+    return `${mins.toString().padStart(2, '0')}:${secs
+      .toString()
+      .padStart(2, '0')}`;
+  }
+};
