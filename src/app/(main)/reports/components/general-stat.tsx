ec0eb7892b880132ac6cbf6951f2fb0118d5f47f
Do Quang Tuan <quangtuan9102003@gmail.com>
2025-12-26 19:00:51 +0700

    fix: Correct typo in class name for grid layout in GeneralStat component

--- src/app/(main)/reports/components/general-stat.tsx
+++ src/app/(main)/reports/components/general-stat.tsx
@@ -39,7 +39,7 @@ const GeneralStat = ({ timeRange }: GeneralStatProps) => {
   }
 
   return (
-    <div className="grid gap-4 md:grid-cols-2 lag: grid-cols-4">
+    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
       <Card>
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
           <CardTitle>Total Time</CardTitle>
